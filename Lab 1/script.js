6c0911f28d690235125c5ffc4c73b156b1788314
Ben Littleton <B.Little1996@gmail.com>
2016-09-19 15:45:05 -0400

    Compiled basic 'hello world' style application.

--- Lab 1/script.js
+++ Lab 1/script.js
@@ -1,15 +1,5 @@
-document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
-  //register the listeners from Phonegap
-  document.addEventListener('pause', onPause, false);
-  document.addEventListener('resume', onResume, false);
-}
 
-function onPause() {
-
-}
-
-function onResume() {
-
+function showConfirmClick(event) {
+  navigator.notification.alert("Text", () => {}, ['Glorious Alert Box'], ['Exit', 'Restart']);
 }
