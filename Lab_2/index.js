67325a39c495a89a26146fbe7db30cf6d91c899d
Ben Littleton <B.Little1996@gmail.com>
2016-09-26 14:56:26 -0400

    Added basic console.log functionality, logs device.properties

--- Lab_2/index.js
+++ Lab_2/index.js
@@ -1,5 +1,30 @@
-document.addEventListener('deviceready', onDeviceReady, false)
+document.addEventListener('deviceready', () => {
+  properties = [
+    device.cordova,
+    device.model,
+    device.platform,
+    device.uuid,
+    device.manufacturer,
+    device.isVirtual,
+    device.serial
+  ]
+  for(var i = 0; i < properties.length; i++) {
+    console.log(properties[i]);
+  }
+}, false)
 
 function onDeviceReady() {
   // ...
+  properties = [
+    device.cordova,
+    device.model,
+    device.platform,
+    device.uuid,
+    device.manufacturer,
+    device.isVirtual,
+    device.serial
+  ]
+  for(var i = 0; i < properties.length; i++) {
+    console.log(properties[i]);
+  }
 }
