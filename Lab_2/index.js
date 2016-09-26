f482d62964bb8acaafba1a8372a84da1aeabfd89
Ben Littleton <B.Little1996@gmail.com>
2016-09-26 15:45:36 -0400

    Completed functionality portion of lab, note that the config.xml file is out of date and you'll have to change it

--- Lab_2/index.js
+++ Lab_2/index.js
@@ -1,30 +1,24 @@
 document.addEventListener('deviceready', () => {
-  properties = [
-    device.cordova,
-    device.model,
-    device.platform,
-    device.uuid,
-    device.manufacturer,
-    device.isVirtual,
-    device.serial
-  ]
-  for(var i = 0; i < properties.length; i++) {
-    console.log(properties[i]);
+  //create an object representing our device from cordova's device plugin!
+  //note the use of strings as keys, this will be used to easily print out this information later on
+  const deviceInfo = {
+    "Device Cordova": device.cordova,
+    "Device Model": device.model,
+    "Device Platform": device.platform,
+    "Device UUID": device.uuid,
+    "Deivce Manufacturer": device.manufacturer,
+    "Virtual Device": device.isVirtual,
+    "Device Serial": device.serial
   }
-}, false)
+  //Now, let's create an array that consists of all our keys from deviceInfo. We will use it to not only determine the number of properties in deviceInfo, but also to print them!
+  let deviceKeys = Object.keys(deviceInfo)
+  //prints out our device info
+  let target = document.getElementById('target')
+  let generatedHTML = ''
+  for(var i = 0; i < deviceKeys.length; i++) {
+    console.log(deviceKeys[i] + ': ' + deviceInfo[deviceKeys[i]])
 
-function onDeviceReady() {
-  // ...
-  properties = [
-    device.cordova,
-    device.model,
-    device.platform,
-    device.uuid,
-    device.manufacturer,
-    device.isVirtual,
-    device.serial
-  ]
-  for(var i = 0; i < properties.length; i++) {
-    console.log(properties[i]);
+    generatedHTML += deviceKeys[i] + ': ' + deviceInfo[deviceKeys[i]] + '<br>'
   }
-}
+  target.innerHTML = generatedHTML
+}, false)
